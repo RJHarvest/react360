528024b6c556f940d9801d96fd2e7745478db83e
Russell Gapasin <russell@fanswifi.com>
2020-06-18 01:38:12 +0800

    Added get route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'build')))
 app.use('/static_assets', express.static(path.join(__dirname, 'static_assets')))
 
+app.get('/', (req, res) => {
+  res.send('build/index.html')
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
